db1a9c511843b59ae04e363286c6e5b90397930b
Baptiste Pauchet <baptiste.pauchet13@gmail.com>
2022-04-27 17:29:13 +0200

    Changement du moyen de connection

--- main.py
+++ main.py
@@ -1,19 +1,33 @@
+from sqlite3 import connect
 from tkinter import*
 import os
 import serial
+port ="/dev/ttyUSB0"
 #fenetre principale
 screen = Tk()
 screen.title("BMW M1 #1 telemetrie")
 screen.minsize(1100,500)
 screen.bg = "white"
 screen.iconphoto(False,PhotoImage(file='image/bmw.png'))
+#Connextion serie
+ConnectSerial = Toplevel()
+ConnectSerial.iconphoto(False,PhotoImage(file='image/bmw.png'))
+ConnectSerial.title("Connextion a l'arduino")
+ConnectSerial.minsize(500,500)
+ConnectSerial.minsize(500,500)
+Titre = Label(ConnectSerial,text="Connextion",font=("arial",30)).pack()
+entry = Entry(ConnectSerial,width=20).pack()
+def Valider(port):
+    port = entry.get()
+    return port
+BoutonValider = Button(ConnectSerial,text="Valider",bg="green").pack()
 #varriable
 VarBoucle = 0
-Telemetrie = serial.Serial()
+Telementrie = serial.Serial(port,9600,timeout=1)
 #fonction
-def ConfigSerial():
-    Telementrie = serial.Serial("/dev/ttyUSB0")
-    print(Telemetrie.name)
+def VerificationConnextion():
+    print("Votre arduino et connecter sur : ")
+    print(Telementrie.name())
 
 #mise en place de l'image central
 IMGNORMAL1 = PhotoImage(file="image/normal1.png")
@@ -54,19 +68,18 @@ LabelImageD = Label(Cadreright,image=IMG100,bg="blue")
 LabelEcartD4 = Label(Cadreright,bg="blue",height="4",width="10")
 #Bouton
 def CommandStart():
-    print(Telemetrie.name)
-    Telemetrie.open()
+    Telementrie.open()
     while VarBoucle == 1:
-        serialdata = Telemetrie.readline()
+        serialdata = Telementrie.readline()
         print(serialdata)
 
  #Menu
 configMenu = Menu(screen,bg="grey",fg="white")
 Option = Menu(configMenu,tearoff=0)
 StartStop = Menu(configMenu,tearoff=0)
-Option.add_command(label="Connexion serie",command=ConfigSerial)
-Option.add_command(label="Config baterie")
-Option.add_command(label="Config temperature")
+Option.add_command(label="Verification de la connextion",command=VerificationConnextion)
+Option.add_command(label="Configuration baterie")
+Option.add_command(label="Configuration temperature")
 StartStop.add_command(label="Start",command=CommandStart)
 StartStop.add_command(label="Stop")
 configMenu.add_cascade(label="Configuration",menu=Option)
