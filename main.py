8b80285737038b416e5c9ee6e4aaf3950c88ce24
Baptiste Pauchet <baptiste.pauchet13@gmail.com>
2022-05-02 21:46:23 +0200

    Mise a jour de l'affichage

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ from tkinter import*
 import os
 import serial
 import time
+#varriable
+VarBoucle = 0
 port ="/dev/ttyUSB0"
 color = "white"
 TextColor = "black"
+Tmax = 8.00
+vide =""
 #fonction de noitoiment des donnes
 def nettoieTemp(L):
     temp=L[4:]
@@ -24,13 +28,11 @@ screen.title("BMW M1 #1 telemetrie")
 screen.minsize(1100,500)
 screen.config(bg = color)
 screen.iconphoto(False,PhotoImage(file='image/bmw.png'))
-#varriable
-VarBoucle = 0
-Telementrie = serial.Serial(port,9600,timeout=1)
+
 #fonction
 def VerificationConnextion():
     print("Votre arduino et connecter sur : ")
-    print(Telementrie.name)
+    print(port)
 
 #mise en place de l'image central
 IMGNORMAL1 = PhotoImage(file="image/normal1.png")
@@ -49,7 +51,7 @@ labelImage = Label(image=IMGcentral)
 CadreLeft= Frame(screen,width=300,height=800,bg = color)
 Cadreright= Frame(screen,width=300,height=800,bg = color)
 
-#mise en forme des cadre:
+
 LabelTitre2 = Label(CadreLeft, text="Temperature",width=10,font=("arial",30),bg = color ,fg = TextColor)
 LabelTitre1 = Label(Cadreright,text="Batterie   ",width=10,font=("arial",30),bg = color,fg = TextColor)
 LabelEcartG1 = Label(CadreLeft,height="17",width="10",bg = color)
@@ -67,6 +69,7 @@ LabelAffichageD2 = Label(Cadreright,width="10",height="2")
 LabelEcartD3 = Label(Cadreright,height="8",width="10",bg = color)
 LabelImageD = Label(Cadreright,image=IMG100,bg = "white")
 LabelEcartD4 = Label(Cadreright,height="4",width="10",bg = color)
+
 #affichage
 def affichagePrincipal():
     CadreLeft.pack(side="left")
@@ -92,30 +95,80 @@ def AffichageTemp():
     LabelEcartG3.pack()
 
 #Menu
-def CommandStart():
-    VarBoucle =+ 1
-    while VarBoucle == 1:
-        time.sleep(1)
-        serialdata = Telementrie.readline()
-        rawdata1 = str(serialdata)
-        
-        temperature = nettoieTemp(rawdata1)
-        temperatureSTR1 =  str(temperature)
-        LabelAffichageG['text'] = temperatureSTR1
-        tension = nettoieTension(rawdata1)
-        TensionSTR1 = str(tension)
-        LabelAffichageD1['text'] = TensionSTR1
-        
-        rawdata2 = str(serialdata)
-        temperature = nettoieTemp(rawdata2)
-        temperatureSTR2 =  str(temperature)
-        LabelAffichageG['text'] = temperatureSTR2
-        tension = nettoieTension(rawdata2)
-        TensionSTR2 = str(tension)
-        LabelAffichageD1['text'] = TensionSTR2
 
-        print("Temperature= ",temperature)
-        print("Tension= ",tension)
+def affichageValeur():
+    Telementrie = serial.Serial(port,9600,timeout=1)
+    serialdata = Telementrie.readline()
+    rawdata1 = str(serialdata)
+    print(rawdata1)
+    temperature = nettoieTemp(rawdata1)
+    tension = nettoieTension(rawdata1) 
+    pourcentage = tension * 100 / Tmax
+    pourcentageSTR = str(pourcentage)
+    temperatureSTR1 =  str(temperature)
+    TensionSTR1 = str(tension)
+    LabelAffichageG['text'] = temperatureSTR1
+    LabelAffichageD1['text'] = TensionSTR1
+    LabelAffichageD2['text'] = pourcentageSTR
+    if temperature >= 17 :
+        LabelImageG['image'] = IMGFROID
+    if temperature <= 17  and temperature >= 30 :
+        LabelImageG['image'] = IMGNORMAL1
+    if temperature <= 30 and temperature >= 45 :
+        LabelImageG['image'] = IMGNORMAL2
+    if temperature <= 45 : 
+        LabelImageG['image'] = IMGCHAUD  
+    
+    if pourcentage >= 1:
+        LabelImageD['image'] = IMG0
+    if pourcentage <= 1 and pourcentage >= 19:
+        LabelImageD['image'] = IMG20
+    if pourcentage <= 20 and pourcentage >= 39:
+        LabelImageD['image'] = IMG40   
+    if pourcentage <= 40 and pourcentage >= 59:
+        LabelImageD['image'] = IMG60
+    if pourcentage <= 60 and pourcentage >= 79:
+        LabelImageD['image'] = IMG80
+    if pourcentage <= 80 and pourcentage >= 100:
+        LabelImageD['image'] = IMG100
+def CommandStart():
+    VarBoucle =+1 
+    while VarBoucle == 1 :
+        affichageValeur()
+        LabelAffichageD1.update()
+        LabelAffichageD2.update()
+        LabelAffichageG.update()
+        LabelImageD.update()
+        LabelImageG.update()
+        time.sleep(1.5)
+        affichageValeur()
+        LabelAffichageD1.update()
+        LabelAffichageD2.update()
+        LabelAffichageG.update()
+        LabelImageD.update()
+        LabelImageG.update()
+        time.sleep(1.5)
+        affichageValeur()
+        LabelAffichageD1.update()
+        LabelAffichageD2.update()
+        LabelAffichageG.update()
+        LabelImageD.update()
+        LabelImageG.update()
+        time.sleep(1.5)
+        affichageValeur()
+
+def reste():
+    LabelImageG['image']= IMGNORMAL1
+    LabelImageD['image'] = IMG100
+    LabelAffichageG['text'] = vide
+    LabelAffichageD1['text'] = vide
+    LabelAffichageD2['text'] = vide
+    LabelAffichageD1.update()
+    LabelAffichageD2.update()
+    LabelAffichageG.update()
+    LabelImageD.update()
+    LabelImageG.update()
+
         
 configMenu = Menu(screen,bg=color,fg = TextColor)
 Option = Menu(configMenu,tearoff=0)
@@ -124,7 +177,7 @@ Option.add_command(label="Verification de la connextion",command=VerificationCon
 Option.add_command(label="Configuration baterie")
 Option.add_command(label="Configuration temperature")
 StartStop.add_command(label="Start",command=CommandStart)
-StartStop.add_command(label="Stop")
+StartStop.add_command(label="Restet" ,command=reste)
 configMenu.add_cascade(label="Configuration",menu=Option)
 configMenu.add_cascade(label="On off",menu=StartStop)
 screen.config(menu=configMenu) 
